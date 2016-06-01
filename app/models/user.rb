74af1116a26a03ec64e61b075e6de2e410aa0bb9
takahiro sahashi <pmen384@gmail.com>
2016-06-01 07:10:06 +0000

    Want、Have機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,11 @@ class User < ActiveRecord::Base
 
   has_many :ownerships , foreign_key: "user_id", dependent: :destroy
   has_many :items ,through: :ownerships
-
+  has_many :wants, class_name: "Want", foreign_key: "user_id", dependent: :destroy
+  has_many :want_items , through: :wants, source: :item
+  has_many :haves, class_name: "Have", foreign_key: "user_id", dependent: :destroy
+  has_many :have_items , through: :haves, source: :item
+  
 
   # 他のユーザーをフォローする
   def follow(other_user)
@@ -31,20 +35,28 @@ class User < ActiveRecord::Base
 
   ## TODO 実装
   def have(item)
+    haves.find_or_created_by(item_id: item.id)
   end
 
   def unhave(item)
+    have = haves.find_by(item_id: item.id)
+    have.destroy if have
   end
 
   def have?(item)
+    have_users.include?(item)
   end
 
   def want(item)
+    wants.find_or_created_by(item_id: item.id)
   end
 
   def unwant(item)
+    want = wants.find_by(item_id: item.id)
+    want.destroy if want
   end
 
   def want?(item)
+    want_users.include?(item)
   end
 end
